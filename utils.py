0f7f728aafaffeccaecf0d76ccd0fbbe824d11d8
Nathaniel Weinstein <nathaniel.weinstein@gmail.com>
2017-02-12 15:14:17 -0800

    Add part of speech to data reading

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ from six.moves import cPickle
 import numpy as np
 import re
 from custom_parser import custom_parse
+import nltk
 
 class TextLoader():
     def __init__(self, data_dir, batch_size, seq_length, encoding='utf-8'):
@@ -15,56 +16,69 @@ class TextLoader():
 
         input_file = os.path.join(data_dir, "input.txt")
         vocab_file = os.path.join(data_dir, "vocab.pkl")
+        tags_file = os.path.join(data_dir, "tags.pkl")
         tensor_file = os.path.join(data_dir, "data.npy")
 
-        if not (os.path.exists(vocab_file) and os.path.exists(tensor_file)):
+        if not (os.path.exists(vocab_file) and os.path.exists(tags_file) and os.path.exists(tensor_file)):
             print("reading text file")
-            self.preprocess(input_file, vocab_file, tensor_file)
+            self.preprocess(input_file, vocab_file, tags_file, tensor_file)
         else:
             print("loading preprocessed files")
-            self.load_preprocessed(vocab_file, tensor_file)
+            self.load_preprocessed(vocab_file, tags_file, tensor_file)
         self.create_batches()
         self.reset_batch_pointer()
 
-    def preprocess(self, input_file, vocab_file, tensor_file):
+    def preprocess(self, input_file, vocab_file, tags_file, tensor_file):
         with codecs.open(input_file, "r", encoding=self.encoding) as f:
             data = f.read()
-        clean_data = custom_parse(data.lower())
-        counter = collections.Counter(clean_data)
-        print(counter)
-        count_pairs = sorted(counter.items(), key=lambda x: -x[1])
-        self.chars, _ = zip(*count_pairs)
+        clean_data = nltk.word_tokenize(data)
+        tagged_data = [t[1] for t in nltk.pos_tag(clean_data)]
+        wordcounter = collections.Counter(clean_data)
+        tagcounter = collections.Counter(tagged_data)
+        wordcount_pairs = sorted(wordcounter.items(), key=lambda x: -x[1])
+        tagcount_pairs = sorted(tagcounter.items(), key=lambda x: -x[1])
+        self.chars, _ = zip(*wordcount_pairs)
+        self.tags, _ = zip(*tagcount_pairs)
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.tag_dict = dict(zip(self.tags, range(len(self.tags))))
         with open(vocab_file, 'wb') as f:
             cPickle.dump(self.chars, f)
-        self.tensor = np.array(list(map(self.vocab.get, clean_data)))
+        with open(tags_file, 'wb') as f:
+            cPickle.dump(self.tags, f)
+        self.tensor = np.array(zip(list(map(self.vocab.get, clean_data)),list(map(self.tag_dict.get, tagged_data))))
         np.save(tensor_file, self.tensor)
 
-    def load_preprocessed(self, vocab_file, tensor_file):
+    def load_preprocessed(self, vocab_file, tags_file, tensor_file):
         with open(vocab_file, 'rb') as f:
             self.chars = cPickle.load(f)
+        with open(tags_file, 'rb') as f:
+            self.tags = cPickle.load(f)
         self.vocab_size = len(self.chars)
         self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.tag_dict = dict(zip(self.tags, range(len(self.tags))))
         self.tensor = np.load(tensor_file)
-        self.num_batches = int(self.tensor.size / (self.batch_size *
+        self.num_batches = int((self.tensor.size / 2) / (self.batch_size *
                                                    self.seq_length))
 
     def create_batches(self):
-        self.num_batches = int(self.tensor.size / (self.batch_size *
+        self.num_batches = int((self.tensor.size / 2) / (self.batch_size *
                                                    self.seq_length))
 
         # When the data (tensor) is too small, let's give them a better error message
         if self.num_batches==0:
             assert False, "Not enough data. Make seq_length and batch_size small."
-
-        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        end_index = self.num_batches * self.batch_size * self.seq_length
+        self.tensor = self.tensor[:end_index, :end_index]
         xdata = self.tensor
         ydata = np.copy(self.tensor)
         ydata[:-1] = xdata[1:]
         ydata[-1] = xdata[0]
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
-        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+        print("SHAPE: {}".format(xdata.shape))
+        xdata = xdata.reshape(-1, self.batch_size, 2)
+        ydata = ydata.reshape(-1, self.batch_size, 2)
+        self.x_batches = np.vsplit(xdata, self.num_batches)
+        self.y_batches = np.vsplit(ydata, self.num_batches)
 
 
     def next_batch(self):
